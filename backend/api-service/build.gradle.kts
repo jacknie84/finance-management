c474c5f58d38ef56e0e05ea63646b759fe8afd4c
jacknie <jacknie8407@gmail.com>
2024-04-05 13:20:36 +0900

    Initialize backend api-service project

---
+++ backend/api-service/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  idea
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.23"
+  kotlin("kapt") version "1.9.23"
+	kotlin("plugin.spring") version "1.9.23"
+  kotlin("plugin.jpa") version "1.9.23"
+}
+
+group = "best.jacknie.finance"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+  kapt("com.querydsl:querydsl-apt::jakarta")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  runtimeOnly("com.h2database:h2")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("com.querydsl:querydsl-jpa::jakarta")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+    freeCompilerArgs += "-Xemit-jvm-type-annotations"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
