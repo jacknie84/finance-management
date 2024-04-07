d91284c9adb7757820c207131f9b4bc7ec480edf
jacknie <jacknie8407@gmail.com>
2024-04-07 23:11:20 +0900

    Add 지출 내역 요약 검색 필터 추가

--- backend/api-service/src/main/kotlin/best/jacknie/finance/spending/log/application/port/SpendingLogsFilter.kt
+++ backend/api-service/src/main/kotlin/best/jacknie/finance/spending/log/application/port/SpendingLogsFilter.kt
@@ -3,6 +3,7 @@ package best.jacknie.finance.spending.log.application.port
 import best.jacknie.finance.core.jpa.querydsl.PredicateProvider
 import best.jacknie.finance.spending.log.domain.QSpendingLogEntity.spendingLogEntity
 import com.querydsl.core.types.ExpressionUtils.allOf
+import com.querydsl.core.types.ExpressionUtils.anyOf
 import com.querydsl.core.types.Predicate
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
@@ -11,6 +12,11 @@ import java.time.DayOfWeek
 import java.time.Month
 
 data class SpendingLogsFilter(
+
+  /**
+   * 요약 검색 목록
+   */
+  var containsSummary: Set<String>?,
 
   /**
    * 연도 목록
@@ -36,6 +42,7 @@ data class SpendingLogsFilter(
 
   override val predicate: Predicate? get() {
     return allOf(
+      anyOf(containsSummary?.map { spendingLogEntity.summary.containsIgnoreCase(it) } ?: emptySet<Predicate>()),
       year?.let { spendingLogEntity.time.year.`in`(it) },
       month?.let { spendingLogEntity.time.month.`in`(it) },
       dayOfMonth?.let { spendingLogEntity.time.dayOfMonth.`in`(it) },
