f291c951d92b6e05f9b021dbac58ad71c979b8bd
jacknie <jacknie8407@gmail.com>
2024-04-16 21:16:21 +0900

    Update spending logs filter

--- backend/api-service/src/main/kotlin/best/jacknie/finance/spending/log/application/port/SpendingLogsFilter.kt
+++ backend/api-service/src/main/kotlin/best/jacknie/finance/spending/log/application/port/SpendingLogsFilter.kt
@@ -8,11 +8,7 @@ import com.querydsl.core.types.ExpressionUtils.anyOf
 import com.querydsl.core.types.Predicate
 import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.JPAExpressions.selectOne
-import jakarta.validation.constraints.Max
-import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.Positive
-import java.time.DayOfWeek
-import java.time.Month
+import java.time.Instant
 
 data class SpendingLogsFilter(
 
@@ -22,49 +18,41 @@ data class SpendingLogsFilter(
   var search001: Set<String>?,
 
   /**
-   * 연도 목록
+   * 검색 시작 시간
    */
-  var year: Set<@Positive @Min(value = 1970) Int>?,
+  var start: Instant?,
 
   /**
-   * 월 목록
+   * 검색 종료 시간
    */
-  var month: Set<Month>?,
-
-  /**
-   * 일 목록
-   */
-  var dayOfMonth: Set<@Min(value = 1) @Max(value = 31) Int>?,
-
-  /**
-   * 요일 목록
-   */
-  var dayOfWeek: Set<DayOfWeek>?,
+  var end: Instant?,
 
 ): PredicateProvider {
 
   override val predicate: Predicate? get() {
     return allOf(
-      anyOf(
-        search001
-          ?.map { spendingLogEntity.summary.containsIgnoreCase(it) }
-          ?.reduce(BooleanExpression::or),
-        search001
-          ?.let {
-            selectOne().from(spendingLogTagEntity)
-              .where(
-                spendingLogTagEntity.log.eq(spendingLogEntity),
-                it
-                  .map { search -> spendingLogTagEntity.tag.containsIgnoreCase(search) }
-                  .reduce(BooleanExpression::or),
-              )
-              .exists()
-          }
-      ),
-      year?.let { spendingLogEntity.time.year.`in`(it) },
-      month?.let { spendingLogEntity.time.month.`in`(it) },
-      dayOfMonth?.let { spendingLogEntity.time.dayOfMonth.`in`(it) },
-      dayOfWeek?.let { spendingLogEntity.time.dayOfWeek.`in`(it) },
+      search001Predicate(),
+      start?.let { spendingLogEntity.time.instant.goe(it) },
+      end?.let { spendingLogEntity.time.instant.lt(it) }
+    )
+  }
+
+  private fun search001Predicate(): Predicate? {
+    return anyOf(
+      search001
+        ?.map { spendingLogEntity.summary.containsIgnoreCase(it) }
+        ?.reduce(BooleanExpression::or),
+      search001
+        ?.let {
+          selectOne().from(spendingLogTagEntity)
+            .where(
+              spendingLogTagEntity.log.eq(spendingLogEntity),
+              it
+                .map { search -> spendingLogTagEntity.tag.containsIgnoreCase(search) }
+                .reduce(BooleanExpression::or),
+            )
+            .exists()
+        }
     )
   }
 }
